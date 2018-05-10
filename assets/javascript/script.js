9ee4df9b63e58c31d8b3762ae70a97105bb94b1d
Robert Shaw <robertshaw87@gmail.com>
2018-05-09 23:00:25 -0700

    add working queue, initial connection behavior

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,13 @@
+var userID, userName, oppID;
+var inQueue = false;
+var currPlaying = false;
+
 var database = firebase.database();
 
 var connectionsRef = database.ref("/connections");
 var connectedStatus = database.ref(".info/connected");
 
-var userID;
+
 var connected = false;
 
 function generateHash() {
@@ -11,19 +15,14 @@ function generateHash() {
         var tempID, exists;
         do {
             exists = false;
-            tempID = Math.floor(Math.random() * 1000000);
+            tempID = userName + "-" + Math.floor(Math.random() * 100);
             snapshot.forEach(function(childsnap){
                 if (childsnap.val().id === tempID.toString()){
                     exists = true;
                 }
             });
         } while (exists);
-        var connectionItem = {
-            id: tempID.toString(),
-            name: "player " + tempID.toString(),
-            status: "queue"
-        }
-        var tempCon = connectionsRef.push(connectionItem);   
+        var tempCon = connectionsRef.push(tempID);   
         tempCon.onDisconnect().remove();
         return tempID;
     });
@@ -31,33 +30,64 @@ function generateHash() {
 
 connectedStatus.on("value", function(snapshot) {
     if (snapshot.val()) {
-        userID = generateHash();
-        
-    }
-});
-
-connectionsRef.on("child_removed", function(snapshot) {
-    if (snapshot.val().statu !== "queue"){
-        var dcMessage = snapshot.val().name + " has disconnected.";
-        database.ref("/chat").push(dcMessage);
+        inQueue = true;
     }
 });
 
 connectionsRef.on("value", function (snapshot) {
-    console.log(snapshot.val())
-    $("#connected").append($("<p>").text(snapshot.val().id))
+   connectionsRef.limitToFirst(2).on("value", function(snap){
+        console.log("first 2: ")
+        console.log(snap.val())
+        snapKeys = Object.keys(snap.val());
+        if (((snapKeys.length) < 2) && !currPlaying) {
+            database.ref("/chat").remove();
+        }
+        var PlayerOne = snap.val()[snapKeys[0]];
+        var PlayerTwo = snap.val()[snapKeys[1]];
+        var tempID = PlayerOne.split(" ").join("");
+        $("#"+tempID).text(PlayerOne + " (Player 1)");
+        tempID = PlayerTwo.split(" ").join("");
+        $("#"+tempID).text(PlayerTwo + " (Player 2)");
+    })
+});
+
+connectionsRef.on("child_removed", function(snapshot) {
+    if (snapshot.val() == oppID){
+        console.log("Disconnect:")
+        console.log(snapshot.val())
+        var dcMessage = snapshot.val() + " has disconnected.";
+        database.ref("/chat").push(dcMessage);
+    }
+    var tempID = snapshot.val().split(" ").join("");
+    $("#"+tempID).remove();
+});
+
+connectionsRef.on("child_added", function (snapshot) {
+    var tempID = snapshot.val().split(" ").join("");
+    $("#connected").append($("<p>").attr("id", tempID).text(snapshot.val()))
+});
+
+$(document).on("click", "#name-submit", function(event){
+    event.preventDefault();
+    if (inQueue){
+    userName = $("#name-input").val().trim();
+    $("#name-input").val("");
+    $("#rps-body").removeClass("hidden");
+    $("#start-username").addClass("hidden");
+    $("#chat-display").scrollTop($("#chat-display").prop("scrollHeight"));
+    userID = generateHash();
+    } else {
+        $("#username-message").text("You're not yet connected! Please try again.")
+    }
 });
 
 $(document).on("click", "#chat-submit", function(event){
     event.preventDefault();
-    
-    var chat = $("#chat-input").val().trim();
+    var chat = userID + ": " + $("#chat-input").val().trim();
     $("#chat-input").val("");
-
-    database.ref("/chat").push(chat);
-    
-    database.ref("/chat").limitToFirst(2).on("value", function(snap){
-    })
+    if (curPlaying) {
+        database.ref("/chat").push(chat);
+    }
 });
 
 database.ref("/chat").on("child_added", function (childSnapshot, prevChildKey) {
